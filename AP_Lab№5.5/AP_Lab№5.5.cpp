d7082c49c4659d1b0f5f5096ad5da09fd33680db
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-10-05 22:39:39 +0300

    Lab5.5

---
+++ AP_Lab№5.5/AP_Lab№5.5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int md = 0; // max depth
+
+int bc(int n, int k, int d)
+{
+    md = max(md, d);
+
+    if (k == 0 || k == n) {
+        return 1;
+    }
+    else {
+        return bc(n - 1, k - 1, d + 1) + bc(n - 1, k, d + 1);
+    }
+}
+
+int main() {
+    int n, k;
+    cout << "n = ";cin >> n;
+    cout << "k = ";cin >> k;
+
+    cout << "Binomal coeficient C(" << n << "," << k << ") = " << bc(n, k, 0) << endl;
+    cout << "Max depth of recurtion: " << md << endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../AP_Lab№5.5/AP_Lab№5.5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+            int n = 10;
+            int k = 5;
+            int expected = 252;
+            md = 0;
+            int result = bc(n, k, 0);
+            Assert::AreEqual(expected, result);
+			Assert::IsTrue(md > 0);
+		}
+	};
+}
